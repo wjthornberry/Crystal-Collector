6f6a3b138399b61eb9e4a7fc1f1dabaa144d179e
Jake Thornberry <jakethornberry@Rocinante.local>
2017-05-23 20:21:41 -0500

    Initial commit

---
+++ assets/javascript/pseudocode.js
@@ -0,0 +1,26 @@
+// Create container for directions to player 
+// Create variables, one for each crystal. Create IDs for each crystal in HTML.     
+// Have button and process to start and restart game 
+// At the start of each game, 
+//   - Game create random number between 19 and 120
+//   - each crystal creates random number 1-12, with each having different random number 
+// Capture player crystal selection on upstroke. 
+// Compute new player total 
+// show new updated amount on screen.    
+// As player presses new key, the new cyrstal button adds to total amount and refreshes on screen 
+// Condition determins if player wins or loses. 
+// - player wins if amount matches 
+// - players loses when player number goes over comp random number 
+// - as player wins or loses, have two counters track total wins and losses 
+// - updated screen with each win or loss 
+// Game automatically refreshes after win/loss game state 
+// Crystals get new amounts. 
+// New comp number randomly chosen 
+// The game restarts whenever the player wins or loses.
+// Reset player score and score counter 
+// Play on...and on.
+// 
+// var GEMS = {},
+// GEMS.btn = $('.btn'),
+// $(GEMS.btns).on('click', functions) {
+// ...this.}
